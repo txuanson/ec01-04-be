4848165063a70b12c8867d5d53633c47de3b5af4
txuanson <tranxuanson2013@gmail.com>
2022-07-28 10:02:54 +0700

    passport authentication & init authorization

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,18 @@
+import { PassportStrategy } from "@nestjs/passport"
+import { AuthService } from "../auth.service"
+import { Strategy } from "passport-local"
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, "local") {
+  constructor(
+    private readonly authService: AuthService
+  ) {
+    super({ usernameField: 'mEmail', passwordField: 'mPassword' })
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.authenticate(email, password);
+    return user;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setTitle('Himitu API')
     .setDescription('E-commerce API for Himitu')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('explorer', app, document);
